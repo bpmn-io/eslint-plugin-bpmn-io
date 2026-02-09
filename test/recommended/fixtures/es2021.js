a3ec18519d29380d7ac7ea27dbe183d38008d59f
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-09 09:39:37 +0100

    feat: support eslint@10

--- test/recommended/fixtures/es2021.js
+++ test/recommended/fixtures/es2021.js
@@ -12,11 +12,14 @@ logicalAnd &&= false; // updated to false
 
 logicalAnd &&= true; // still false
 
+console.log(logicalAnd);
 
 let logicalOr = false;
 logicalOr ||= true; // updated to true
 
 logicalOr ||= 'new'; // still true
+
+console.log(logicalOr);
 
 const a = new WeakRef({});
 a.deref();
