4cc4d165db69adc5f09bd8301815df10da48328d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-09 09:35:06 +0100

    FIXME: undo once eslint-plugin-react supports eslint@10

--- test/jsx/eslint.config.js
+++ test/jsx/eslint.config.js
@@ -1,5 +1,12 @@
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
 
 export default [
-  ...bpmnIoPlugin.configs.jsx
+  ...bpmnIoPlugin.configs.jsx,
+  {
+    rules: {
+      'react/jsx-tag-spacing': 'off',
+      'react/jsx-equals-spacing': 'off',
+      'react/jsx-curly-spacing': 'off'
+    }
+  }
 ];
